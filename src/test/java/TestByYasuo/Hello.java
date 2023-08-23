cdf45446d319293cfaa6abb542e86d38675ea472
psg <tkdrjsiop@gmail.com>
2023-08-23 16:28:18 +0900

    TestByYasuo

---
+++ src/test/java/TestByYasuo/Hello.java
@@ -0,0 +1,11 @@
+package TestByYasuo;
+
+public class Hello {
+
+    public static void main(String[]args){
+        System.out.println("hello?");
+
+}
+
+}
+
---
+++ src/test/java/TestByYasuo/JavaBasicDataType.java
@@ -0,0 +1,4 @@
+package TestByYasuo;
+
+public class JavaBasicDataType {
+}
