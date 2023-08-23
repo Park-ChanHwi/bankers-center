0730cd0aea1013ddb7d3502f667548e437e3668b
Park-ChanHwi <x4cf7c3an2t@naver.com>
2023-08-23 14:50:15 +0900

    8-23 004

--- src/main/java/bankers/fisa/controller/HaedamTestController.java
+++ src/main/java/bankers/fisa/controller/HaedamTestController.java
@@ -1,12 +1,6 @@
 package bankers.fisa.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import bankers.fisa.entity.VM;
-import bankers.fisa.entity.ckey.VMckey;
-import bankers.fisa.repository.VMRepository;
 
 @RestController
 public class HaedamTestController {
