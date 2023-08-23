da5e60a43c1f6dbf80426e8414ff35f2aac5f3c8
Park-ChanHwi <x4cf7c3an2t@naver.com>
2023-08-23 16:31:45 +0900

    8-23 007

--- src/main/java/bankers/fisa/controller/MainController.java
+++ src/main/java/bankers/fisa/controller/MainController.java
@@ -1,5 +1,21 @@
 package bankers.fisa.controller;
 
-public class MainController {
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/controller")
+public class MainController {
+	
+	@PostMapping("/login")
+	public String getBankersEmp(@RequestParam("loginID") String loginID) {
+		
+		String result = "false";
+		if(loginID.equals("asd123")) {
+			result = "true";
+		}
+		return result;
+	}
 }
