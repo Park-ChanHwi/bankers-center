e87314771ee7be528d2aff33b210a2d5c1fe421a
psg <tkdrjsiop@gmail.com>
2023-08-24 09:43:37 +0900

    ArrayTest

---
+++ src/test/java/TestByYasuo/ArrayTest.java
@@ -0,0 +1,11 @@
+package TestByYasuo;
+
+
+import java.util.Arrays;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[] array = {'아',1,'가','나',2};
+        System.out.println();
+    }
+}
