fec414ce54aa7c32c924df0c76c593790eebe266
Park-ChanHwi <x4cf7c3an2t@naver.com>
2023-08-25 17:08:48 +0900

    8-25 8시 merge

--- src/main/java/bankers/fisa/controller/MainController.java
+++ src/main/java/bankers/fisa/controller/MainController.java
@@ -1,5 +1,8 @@
 package bankers.fisa.controller;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import bankers.fisa.entity.CustEmp;
 import bankers.fisa.entity.VM;
+import bankers.fisa.entity.ckey.VMckey;
 import bankers.fisa.repository.CustEmpRepository;
 import bankers.fisa.repository.VMRepository;
 
@@ -21,7 +25,40 @@ public class MainController {
 	private final CustEmpRepository custEmpRepository = null;
 	@Autowired
 	private final VMRepository vmRepository = null;
-
+	
+	@PostMapping("/getvm")
+	public String getvm(
+			@RequestParam("id") String id,
+			@RequestParam("vmnumber") String vmnumber) {
+		String result = new String();
+		System.out.println(vmRepository.findLatestVM(vmnumber).toString());
+		
+		return result;
+	}
+	
+	@PostMapping("/vmcreate")
+	public String vmcreate(
+			@RequestParam("vmname") String vmname,
+			@RequestParam("catalType") String catalType,
+			@RequestParam("creater") String creater) {
+		try {
+			int custEmpNumber = custEmpRepository.findById(creater).get().getCust_emp_number();
+			
+			DateFormat dateFormat = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss");
+			String date = dateFormat.format(new Date());
+			
+			System.out.println(date);
+			VMckey vmckey = new VMckey(vmRepository.getMAXNumber() + 1, date);
+			VM newVM = new VM(vmckey, vmname, catalType, "preparing", "OFF", custEmpNumber);
+			
+			vmRepository.save(newVM);
+			
+			return "true";
+		} catch (Exception e) {
+			return "false";
+		}
+	}
+	
 	@PostMapping("/custemp")
 	public String custEmp(@RequestParam("custEmpNumber") int custEmpNumber) {
 		try {
